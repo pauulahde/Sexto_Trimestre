58f7b1d224a0822f36bd56ec78a4c93767fa2765
Paula Rodriguez <rodriguezdiazmariapaula@gmail.com>
2026-03-21 17:21:38 +0000

    listas

---
+++ MiPrimerAngular/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+Bienvenidos ='Hola Paula';
+
+Estudiantes =['Cuadernos','Gorras','Gafas']
+
+}
